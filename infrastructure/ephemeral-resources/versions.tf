63e7faf08b014d8497d1289ad5d60bdb724856cc
Benhur A. Silva <benhur.araujo.silva@gmail.com>
2023-10-21 15:11:29 -0300

    feat(ephemeral): create argocd tf manifests

--- infrastructure/ephemeral-resources/versions.tf
+++ infrastructure/ephemeral-resources/versions.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.12.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11.0"
+    }
   }
 
   backend "s3" {
@@ -21,6 +25,19 @@ provider "aws" {
     tags = {
       project = local.project
       owners  = "the-devops-guys"
+    }
+  }
+}
+
+provider "helm" {
+  # https://registry.terraform.io/providers/hashicorp/helm/latest/docs#exec-plugins
+  kubernetes {
+    host                   = aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks.name]
     }
   }
 }
